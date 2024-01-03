a617674f737423749e81a1a25404074c76ad89ff
ecorbisierSimplon <144805223+ecorbisierSimplon@users.noreply.github.com>
2024-01-03 22:34:42 +0100

    Eric/Pagination

---
+++ src/icon.ts
@@ -0,0 +1,63 @@
+export function displayIcon(): string {
+  return `
+    <div class='icons'>
+        <div><span class='icon icon_add'></span>icon_add</div>
+        <div><span class='icon icon_alert'></span>icon_alert</div>
+        <div><span class='icon icon_arrow_sm_down'></span>icon_arrow_sm_down</div>
+        <div><span class='icon icon_arrow_sm_left'></span>icon_arrow_sm_left</div>
+        <div><span class='icon icon_arrow_sm_right'></span>icon_arrow_sm_right</div>
+        <div><span class='icon icon_arrow_sm_up'></span>icon_arrow_sm_up</div>
+        <div><span class='icon icon_badges'></span>icon_badges</div>
+        <div><span class='icon icon_body'></span>icon_body</div>
+        <div><span class='icon icon_cards'></span>icon_cards</div>
+        <div><span class='icon icon_cautionsymbol'></span>icon_cautionsymbol</div>
+        <div><span class='icon icon_check'></span>icon_check</div>
+        <div><span class='icon icon_checkmark'></span>icon_checkmark</div>
+        <div><span class='icon icon_clothing'></span>icon_clothing</div>
+        <div><span class='icon icon_collection'></span>icon_collection</div>
+        <div><span class='icon icon_create_avatar'></span>icon_create_avatar</div>
+        <div><span class='icon icon_double'></span>icon_double</div>
+        <div><span class='icon icon_download'></span>icon_download</div>
+        <div><span class='icon icon_event'></span>icon_event</div>
+        <div><span class='icon icon_eye_closed'></span>icon_eye_closed</div>
+        <div><span class='icon icon_eye_open'></span>icon_eye_open</div>
+        <div><span class='icon icon_female_symbol'></span>icon_female_symbol</div>
+        <div><span class='icon icon_filter_height_large'></span>icon_filter_height_large</div>
+        <div><span class='icon icon_filter_height_medium'></span>icon_filter_height_medium</div>
+        <div><span class='icon icon_filter_height_small'></span>icon_filter_height_small</div>
+        <div><span class='icon icon_filter_weight_large'></span>icon_filter_weight_large</div>
+        <div><span class='icon icon_filter_weight_medium'></span>icon_filter_weight_medium</div>
+        <div><span class='icon icon_filter_weight_small'></span>icon_filter_weight_small</div>
+        <div><span class='icon icon_friends'></span>icon_friends</div>
+        <div><span class='icon icon_gender'></span>icon_gender</div>
+        <div><span class='icon icon_hamburger'></span>icon_hamburger</div>
+        <div><span class='icon icon_home'></span>icon_home</div>
+        <div><span class='icon icon_journal'></span>icon_journal</div>
+        <div><span class='icon icon_joystick'></span>icon_joystick</div>
+        <div><span class='icon icon_male_symbol'></span>icon_male_symbol</div>
+        <div><span class='icon icon_messages'></span>icon_messages</div>
+        <div><span class='icon icon_minus'></span>icon_minus</div>
+        <div><span class='icon icon_multiply'></span>icon_multiply</div>
+        <div><span class='icon icon_notepage'></span>icon_notepage</div>
+        <div><span class='icon icon_plus'></span>icon_plus</div>
+        <div><span class='icon icon_points_champ'></span>icon_points_champ</div>
+        <div><span class='icon icon_points_game'></span>icon_points_game</div>
+        <div><span class='icon icon_pokeball'></span>icon_pokeball</div>
+        <div><span class='icon icon_pokedex_added'></span>icon_pokedex_added</div>
+        <div><span class='icon icon_prize_bronze'></span>icon_prize_bronze</div>
+        <div><span class='icon icon_prize_gold'></span>icon_prize_gold</div>
+        <div><span class='icon icon_prize_silver'></span>icon_prize_silver</div>
+        <div><span class='icon icon_refresh'></span>icon_refresh</div>
+        <div><span class='icon icon_remove'></span>icon_remove</div>
+        <div><span class='icon icon_search'></span>icon_search</div>
+        <div><span class='icon icon_settings'></span>icon_settings</div>
+        <div><span class='icon icon_trade'></span>icon_trade</div>
+        <div><span class='icon icon_videogame'></span>icon_videogame</div>
+        <div><span class='icon icon_wishlist'></span>icon_wishlist</div>
+        <div><span class='icon icon_wrench'></span>icon_wrench</div>
+        <div><span class='icon icon_news'></span>icon_news</div>
+        <div><span class='icon icon_mobile'></span>icon_mobile</div>
+        <div><span class='icon icon_go'></span>icon_go</div>
+    </div>
+    `;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,44 @@
+import { getCookie, setCookie } from "typescript-cookie";
 import { getPokemonJson, getPokemon } from "./pokemon_list";
+import { htmlHome } from "./html/pokemonList";
+import { displayIcon } from "./icon";
 import "./scss/style.scss";
-import { htmlHome } from "./html/pokemonList.ts";
+
+export const secureCookie: boolean = false;
+export const hostname: string = window.location.hostname;
+let numPage: number = 0;
+const limit: number = Number(getCookie("limit"));
 
 const params: string[] = window.location.pathname.split("/");
+let offset: number = 0;
 console.log(params);
 
-const pokemonJson: any = await getPokemonJson(
-  "https://pokeapi.co/api/v2/pokemon"
+let pokemonJson: any = await getPokemonJson(
+  `https://pokeapi.co/api/v2/pokemon/`
 );
+let htmlDisplay: any = await getPokemon(pokemonJson, 1, limit);
 
-const htmlPokemon: any = await getPokemon(pokemonJson);
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = htmlHome(
-    htmlPokemon
-);
+if (!getCookie("limit")) {
+  setCookie("limit", 20, { expires: 365, secure: secureCookie });
+}
+if (!getCookie("display")) {
+  setCookie("display", "block", { expires: 365, secure: secureCookie });
+}
+
+if (params[1] === "page") {
+  numPage = Number(params[2]);
+  numPage = numPage == 0 ? 1 : numPage;
+  const page = numPage <= 1 ? 0 : numPage - 1;
+  offset = page * limit;
+
+  pokemonJson = await getPokemonJson(
+    `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+  );
+
+  htmlDisplay = await getPokemon(pokemonJson, numPage, limit);
+} else if (params[1] === "icon") {
+  htmlDisplay = displayIcon();
+}
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML =
+  htmlHome(htmlDisplay);
