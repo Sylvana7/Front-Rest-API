f4b51c208db328862b391489555696a79565c4b6
Sylvana7 <Sylvana.nirina@laposte.net>
2024-01-03 10:53:13 +0100

    Display_name

--- src/pokemon_list.ts
+++ src/pokemon_list.ts
@@ -1,20 +1,23 @@
-export const getPokemon = async (url: string):  Promise<void> => {
-    const container: Response = await fetch(url);
-    const pokemon: any = await container.json();
-    console.log("pokemon");
-    console.log(pokemon.results);
-    for (let array of pokemon.results) {
-        const name = array.name;
-        console.log(name);
-        
-        const url = array.url
-        console.log(url)
-        
-        }
-       
-        let params = window.location.pathname;
-            console.log(params);
-        }
-       
+export const getPokemon = async (url: string): Promise<string> => {
+  const container: Response = await fetch(url);
+  const pokemon: any = await container.json();
+  console.log("pokemon");
+  console.log(pokemon.results);
 
+  const hostname: string = window.location.hostname;
+  let html: string = "";
 
+  for (let array of pokemon.results) {
+    const name = array.name;
+    console.log(name);
+
+    const url = array.url;
+    console.log(url);
+    html += `<div>
+        <a href=${hostname} + "/pokemon/1">
+        <h3>${name}</h3>
+        </a>
+        </div>`;
+  }
+  return html;
+};
