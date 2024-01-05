733e24bf3f86c3753fd3c8539c7d386e7e2a7046
Sylvana7 <Sylvana.nirina@laposte.net>
2024-01-05 10:38:43 +0100

    Sylvana/Add_POST

--- src/ts/routes/pokemonList.ts
+++ src/ts/routes/pokemonList.ts
@@ -1,45 +1,8 @@
-import { Pagination } from "../services/paginations";
 import { fetchPokemon } from "../services/fetch";
 import { Routes } from "./routes";
 import { getCookie } from "typescript-cookie";
 import { hostname } from "../../main";
-
-export class PaginationPokemon {
-  private pagination: any;
-  private currentPage: number;
-
-  constructor(public readonly maxPokemon: number) {
-    this.currentPage = Routes.getNumPage();
-  }
-  public async getPaginationPokemon(): Promise<string> {
-    return await this.init();
-  }
-  private async init(): Promise<string> {
-    this.pagination = new Pagination(this.maxPokemon, this.currentPage);
-    return await this.pagination.getDisplay(
-      this.firstPage(),
-      this.previousPage(),
-      this.numPage(),
-      this.nextPage(),
-      this.lastPage()
-    );
-  }
-  private firstPage(): string {
-    return `<a href='${hostname}/page/1' class='$classNav' title='first page'><span class='icon double icon_arrow_sm_left'></span><span class='icon icon_arrow_sm_left'></span></a>`;
-  }
-  private previousPage(): string {
-    return `<a href='${hostname}/page/$num' class='$classNav' title='previous page'><span class='icon icon_arrow_sm_left'></span></a>`;
-  }
-  private numPage(): string {
-    return `<a href='${hostname}/page/$num' class='$classNav'>$num</a>`;
-  }
-  private nextPage(): string {
-    return `<a href='${hostname}/page/$num' class='$classNav' title='next page'><span class='icon icon_arrow_sm_right'></span></a>`;
-  }
-  private lastPage(): string {
-    return `<a href='${hostname}/page/$num' class='$classNav' title='last page'><span class='icon double icon_arrow_sm_right'></span><span class='icon icon_arrow_sm_right'></span></a>`;
-  }
-}
+import { PaginationPokemon } from "./pokemonPagination";
 
 export class ListPokemon {
   private fetchPokemon: any;
@@ -68,7 +31,9 @@ export class ListPokemon {
     const pokemon: any = this.fetchPokemon.results;
     this.navigation = await pagination.getPaginationPokemon();
 
-    let html: string = `<div class='title'><h2>Pokemon list</h2></div>`;
+    let html: string = "";
+    html += `<form method='POST' action='${hostname}/search' class='pokemon__search'><input type='text' placeholder='Search...' name='pokemon' required><button type='submit'><i class='fa fa-search'></i></button></form>`;
+    html += `<div class='title'><h2>Pokemon list</h2></div>`;
     html += this.navigation;
     html += "<div class='pokemon__list'>";
 
